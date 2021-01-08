92421ebe15024196a01d95956487747d367dcbfb
joantolos <joan.tolos@gmail.com>
2021-01-08 16:09:44 +0100

    Adding delay

--- src/main/java/com/joantolos/kata/words/Words.java
+++ src/main/java/com/joantolos/kata/words/Words.java
@@ -1,16 +1,19 @@
 package com.joantolos.kata.words;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Words {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String input;
         do {
-            System.out.println("\nEscriu: > ");
+            System.out.println("\nEscriu: >");
             input = new Scanner(System.in).nextLine();
             if(!input.equals("exit")) {
-                System.out.println("Has escrit: " + input);
+                System.out.println("Has escrit: \n . . .");
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println(input);
             }
         } while (!input.equals("exit"));
     }
